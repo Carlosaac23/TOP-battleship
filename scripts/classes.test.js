968bc03ec951b8907d7973ec5f8f3a3761c9c431
Carlos <carlosaac232001@gmail.com>
2025-11-18 16:53:59 -0500

    test: add test for placeShip method from Gameboard class and add comments for improve understanding

--- scripts/classes.test.js
+++ scripts/classes.test.js
@@ -1,5 +1,6 @@
-import { Ship } from './classes.js';
+import { Gameboard, Ship } from './classes.js';
 
+// Testing Ship class
 test('A new ship should not be sunk', () => {
   const ship = new Ship(3);
   const isSunk = ship.isSunk();
@@ -25,4 +26,24 @@ test('A ship with length of three and three hits should be sunk', () => {
   const isSunk = ship.isSunk();
 
   expect(isSunk).toBe(true);
+});
+
+// Testing Gameboard class
+test('Place a horizontal ship of length 3 in a valid position', () => {
+  const ship = new Ship(3);
+  const board = new Gameboard();
+
+  board.placeShip(ship, [2, 3], 'horizontal');
+  const cell1 = board.getCell(2, 3);
+  const cell2 = board.getCell(2, 4);
+  const cell3 = board.getCell(2, 5);
+
+  expect(cell1.state).toBe('ship');
+  expect(cell1.ship).toBe(ship);
+
+  expect(cell2.state).toBe('ship');
+  expect(cell2.ship).toBe(ship);
+
+  expect(cell3.state).toBe('ship');
+  expect(cell3.ship).toBe(ship);
 });
