d240aba74b94d5e2735b2579bb264b3274ba8cbc
Carlos <carlosaac232001@gmail.com>
2025-11-18 17:55:48 -0500

    test: add test placing a ship in vertical orientation

--- scripts/classes.test.js
+++ scripts/classes.test.js
@@ -47,3 +47,26 @@ test('Place a horizontal ship of length 3 in a valid position', () => {
   expect(cell3.state).toBe('ship');
   expect(cell3.ship).toBe(ship);
 });
+
+test('Place a vertical ship of length 4 in a valid position', () => {
+  const ship = new Ship(4);
+  const board = new Gameboard();
+
+  board.placeShip(ship, [3, 4], 'vertical');
+  const cell1 = board.getCell(3, 4);
+  const cell2 = board.getCell(4, 4);
+  const cell3 = board.getCell(5, 4);
+  const cell4 = board.getCell(6, 4);
+
+  expect(cell1.state).toBe('ship');
+  expect(cell1.ship).toBe(ship);
+
+  expect(cell2.state).toBe('ship');
+  expect(cell2.ship).toBe(ship);
+
+  expect(cell3.state).toBe('ship');
+  expect(cell3.ship).toBe(ship);
+
+  expect(cell4.state).toBe('ship');
+  expect(cell4.ship).toBe(ship);
+});
