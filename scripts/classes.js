6aaa0ffd88326b87e4b1845b2252859851022e6e
Carlos <carlosaac232001@gmail.com>
2025-11-18 16:54:29 -0500

    feat: add length private property to Ship class

--- scripts/classes.js
+++ scripts/classes.js
@@ -1,21 +1,27 @@
 export class Ship {
-  constructor(length) {
-    this.length = length;
-  }
-
+  #length;
   #hits = 0;
+
+  constructor(length) {
+    this.#length = length;
+  }
 
   hit() {
     this.#hits++;
   }
 
   isSunk() {
-    return this.#hits === this.length;
+    return this.#hits === this.#length;
   }
 }
 
 export class Gameboard {
   constructor() {}
+
+  // initialPosition is an array [row, col]
+  placeShip(ship, initialPosition, direction) {}
+
+  getCell(row, col) {}
 
   receiveAttack() {}
 }
