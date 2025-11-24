549ead1d526a167b419e0d4da7098557f372d2d5
Carlos <carlosaac232001@gmail.com>
2025-11-24 18:06:28 -0500

    feat: create startGame function to test the game rendering the board

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,24 @@
-import { ComputerPlayer, Player, Ship } from './classes.js';
+import { ComputerPlayer, Player } from './classes.js';
 
-const testShip = new Ship();
-const testPlayer = new Player('Carlos');
-const testComputerPlayer = new ComputerPlayer();
+const btn = document.querySelector('.start-game');
+const container = document.querySelector('.game-container');
+btn.addEventListener('click', startGame);
 
-console.log(testShip);
-console.log(testPlayer);
-console.log(testComputerPlayer);
+function startGame() {
+  const testPlayer = new Player('Carlos');
+  const testComputerPlayer = new ComputerPlayer();
+  btn.style.display = 'none';
+  const gameboard = testPlayer.board.board;
+  console.log(gameboard);
+  gameboard.forEach(row => {
+    const boardEl = document.createElement('div');
+    boardEl.classList.add('board');
+    row.forEach(cell => {
+      const cellEl = document.createElement('div');
+      cellEl.classList.add('cell');
+      cellEl.textContent = cell.state;
+      boardEl.appendChild(cellEl);
+      container.appendChild(boardEl);
+    });
+  });
+}
