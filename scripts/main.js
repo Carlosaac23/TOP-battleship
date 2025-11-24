0e496f10ad485576f8c7a6ff7477a580ce13b2cf
Carlos <carlosaac232001@gmail.com>
2025-11-24 18:30:45 -0500

    feat: add dynamic display to computer container too

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,34 @@
 import { ComputerPlayer, Player } from './classes.js';
 
 const btn = document.querySelector('.start-game');
-const container = document.querySelector('.game-container');
+const playerContainer = document.querySelector('.player__container');
+const playerName = document.querySelector('.player__container-name');
+const playerBoard = document.querySelector('.player__container-board');
+
+const computerContainer = document.querySelector('.computer__container');
+
 btn.addEventListener('click', startGame);
 
 function startGame() {
   const testPlayer = new Player('Carlos');
   const testComputerPlayer = new ComputerPlayer();
   btn.style.display = 'none';
+  playerContainer.style.display = 'flex';
+  computerContainer.style.display = 'flex';
+  playerName.textContent = testPlayer.name;
+  playerBoard.style.display = 'grid';
+
   const gameboard = testPlayer.board.board;
   console.log(gameboard);
   gameboard.forEach(row => {
     const boardEl = document.createElement('div');
-    boardEl.classList.add('board');
+
     row.forEach(cell => {
       const cellEl = document.createElement('div');
       cellEl.classList.add('cell');
       cellEl.textContent = cell.state;
       boardEl.appendChild(cellEl);
-      container.appendChild(boardEl);
+      playerBoard.appendChild(boardEl);
     });
   });
 }
