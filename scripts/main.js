39663cc227eb4f1eb615c9f2fadbe4046f3ad3f6
Carlos <carlosaac232001@gmail.com>
2025-11-26 22:47:23 -0500

    feat: select restart button and show when a game is live

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,25 @@
 import { ComputerPlayer, Player } from './classes.js';
 
-const btn = document.querySelector('.start-game');
+const startGameBtn = document.querySelector('.start-game-btn');
 const playerContainer = document.querySelector('.player__container');
 const playerName = document.querySelector('.player__container-name');
 const playerBoard = document.querySelector('.player__container-board');
 
 const computerContainer = document.querySelector('.computer__container');
 
-btn.addEventListener('click', startGame);
+const restartGameBtn = document.querySelector('.restart-game-btn');
+
+startGameBtn.addEventListener('click', startGame);
 
 function startGame() {
   const testPlayer = new Player('Carlos');
   const testComputerPlayer = new ComputerPlayer();
-  btn.style.display = 'none';
+  startGameBtn.style.display = 'none';
   playerContainer.style.display = 'flex';
   computerContainer.style.display = 'flex';
   playerName.textContent = testPlayer.name;
   playerBoard.style.display = 'grid';
+  restartGameBtn.style.display = 'block';
 
   const gameboard = testPlayer.board.board;
   console.log(gameboard);
