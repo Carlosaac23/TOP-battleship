db94c3a4309ee567e393042c659ff4bdf9fbbbad
Carlos <carlosaac232001@gmail.com>
2025-11-26 22:55:25 -0500

    feat: select computer board and render computer gameboard into container

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,7 @@ const playerName = document.querySelector('.player__container-name');
 const playerBoard = document.querySelector('.player__container-board');
 
 const computerContainer = document.querySelector('.computer__container');
+const computerBoard = document.querySelector('.computer__container.board');
 
 const restartGameBtn = document.querySelector('.restart-game-btn');
 
@@ -21,9 +22,8 @@ function startGame() {
   playerBoard.style.display = 'grid';
   restartGameBtn.style.display = 'block';
 
-  const gameboard = testPlayer.board.board;
-  console.log(gameboard);
-  gameboard.forEach(row => {
+  const playerGameboard = testPlayer.board.board;
+  playerGameboard.forEach(row => {
     const boardEl = document.createElement('div');
 
     row.forEach(cell => {
@@ -32,6 +32,22 @@ function startGame() {
       cellEl.textContent = cell.state;
       boardEl.appendChild(cellEl);
       playerBoard.appendChild(boardEl);
+    });
+  });
+
+  const computerGameboard = testComputerPlayer.board.board;
+  computerGameboard.forEach(row => {
+    const boardEl = document.createElement('div');
+    console.log(boardEl);
+    console.log(row);
+
+    row.forEach(cell => {
+      console.log(cell);
+      const cellEl = document.createElement('div');
+      cellEl.classList.add('cell');
+      cellEl.textContent = cell.state;
+      boardEl.appendChild(cellEl);
+      computerBoard.appendChild(boardEl);
     });
   });
 }
