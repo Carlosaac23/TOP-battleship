6be76daeb46f2414352bc0263ce34ff2657a6cf0
Carlos <carlosaac232001@gmail.com>
2025-11-28 16:38:16 -0500

    feat: select input-name container and input

--- scripts/dom.js
+++ scripts/dom.js
@@ -3,7 +3,6 @@ import { finishGame } from './functions.js';
 import { main } from './main.js';
 
 export const gameContainer = document.querySelector('.game-container');
-export const startGameBtn = document.querySelector('.start-game-btn');
 export const playerContainer = document.querySelector('.player__container');
 export const playerName = document.querySelector('.player__container-name');
 export const playerBoard = document.querySelector('.player__container-board');
@@ -13,11 +12,15 @@ export const computerBoard = document.querySelector(
   '.computer__container-board'
 );
 
+export const startGameBtn = document.querySelector('.start-game-btn');
 export const restartGameBtn = document.querySelector('.restart-game-btn');
 export const finishGameBtn = document.querySelector('.finish-game-btn');
 
 startGameBtn.addEventListener('click', main);
 finishGameBtn.addEventListener('click', finishGame);
 
-export const humanPlayer = new Player('Carlos');
+export const inputNameContainer = document.querySelector('.name-container');
+export const inputName = document.querySelector('.name-input');
+
+export const humanPlayer = new Player(inputName.value);
 export const computerPlayer = new ComputerPlayer();
