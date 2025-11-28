96f5a133ed76b698789010d8145db4fc13dba861
Carlos <carlosaac232001@gmail.com>
2025-11-28 16:12:39 -0500

    refactor: move all function here

---
+++ scripts/functions.js
@@ -0,0 +1,101 @@
+import { Ship } from './classes.js';
+import {
+  computerBoard,
+  computerContainer,
+  computerPlayer,
+  finishGameBtn,
+  gameContainer,
+  humanPlayer,
+  playerBoard,
+  playerContainer,
+  playerName,
+  restartGameBtn,
+  startGameBtn,
+} from './dom.js';
+
+function renderPlayerBoard() {
+  playerContainer.style.display = 'flex';
+  playerName.textContent = humanPlayer.name;
+  playerBoard.innerHTML = ''; // Prevent re-render again the board after finish the game
+  playerBoard.style.display = 'grid';
+
+  const playerGameboard = humanPlayer.board.board;
+  playerGameboard.forEach(row => {
+    const boardEl = document.createElement('div');
+
+    row.forEach(cell => {
+      const cellEl = document.createElement('div');
+      cellEl.classList.add('cell', `cell-${cell.state}`);
+      boardEl.appendChild(cellEl);
+      playerBoard.appendChild(boardEl);
+    });
+  });
+}
+
+function renderComputerBoard() {
+  computerContainer.style.display = 'flex';
+  computerBoard.innerHTML = ''; // Prevent re-render again the board after finish the game
+  computerBoard.style.display = 'grid';
+
+  const computerGameboard = computerPlayer.board.board;
+  computerGameboard.forEach(row => {
+    const boardEl = document.createElement('div');
+    console.log(row);
+
+    row.forEach(cell => {
+      const cellEl = document.createElement('div');
+      cellEl.classList.add('cell', `cell-${cell.state}`);
+      boardEl.appendChild(cellEl);
+      computerBoard.appendChild(boardEl);
+    });
+  });
+}
+
+export function startGame() {
+  startGameBtn.style.display = 'none';
+  restartGameBtn.style.display = 'block';
+  finishGameBtn.style.display = 'block';
+  gameContainer.style.display = 'flex';
+
+  placeShipRandomly(humanPlayer.board);
+  placeShipRandomly(computerPlayer.board);
+  renderPlayerBoard();
+  renderComputerBoard();
+}
+
+export function finishGame() {
+  gameContainer.style.display = 'none';
+  startGameBtn.style.display = 'block';
+  humanPlayer.board.reset();
+  computerPlayer.board.reset();
+}
+
+function restartGame() {}
+
+function placeShipRandomly(board) {
+  const carrier = new Ship(5);
+  const battleship = new Ship(4);
+  const cruiser = new Ship(3);
+  const submarine = new Ship(3);
+  const destroyer = new Ship(2);
+  const ships = [carrier, battleship, cruiser, submarine, destroyer];
+
+  ships.forEach(ship => {
+    let placed = false;
+
+    while (!placed) {
+      try {
+        const MAX_ROW_COL_BOARD = 10;
+        const randomRow = Math.floor(Math.random() * MAX_ROW_COL_BOARD);
+        const randomCol = Math.floor(Math.random() * MAX_ROW_COL_BOARD);
+        const orientations = ['vertical', 'horizontal'];
+        const randomOrientation =
+          orientations[Math.floor(Math.random() * orientations.length)];
+        board.placeShip(ship, [randomRow, randomCol], randomOrientation);
+        placed = true;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  });
+}
