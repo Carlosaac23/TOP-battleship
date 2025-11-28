3525e10c777e703f2ea20d42295bdf9fb0bc82c5
Carlos <carlosaac232001@gmail.com>
2025-11-28 16:12:20 -0500

    refactor: move all dom selectors here

---
+++ scripts/dom.js
@@ -0,0 +1,23 @@
+import { ComputerPlayer, Player } from './classes.js';
+import { finishGame } from './functions.js';
+import { main } from './main.js';
+
+export const gameContainer = document.querySelector('.game-container');
+export const startGameBtn = document.querySelector('.start-game-btn');
+export const playerContainer = document.querySelector('.player__container');
+export const playerName = document.querySelector('.player__container-name');
+export const playerBoard = document.querySelector('.player__container-board');
+
+export const computerContainer = document.querySelector('.computer__container');
+export const computerBoard = document.querySelector(
+  '.computer__container-board'
+);
+
+export const restartGameBtn = document.querySelector('.restart-game-btn');
+export const finishGameBtn = document.querySelector('.finish-game-btn');
+
+startGameBtn.addEventListener('click', main);
+finishGameBtn.addEventListener('click', finishGame);
+
+export const humanPlayer = new Player('Carlos');
+export const computerPlayer = new ComputerPlayer();
